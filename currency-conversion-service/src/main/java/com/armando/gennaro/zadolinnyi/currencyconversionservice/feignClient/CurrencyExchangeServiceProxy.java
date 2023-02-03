aa52ef6b5e4e2340b018e7ef1f707b807e01469d
armares2001 <armares2001@libero.it>
2023-02-03 01:25:08 +0100

    changed proxy conversion path in zuul

--- currency-conversion-service/src/main/java/com/armando/gennaro/zadolinnyi/currencyconversionservice/feignClient/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/armando/gennaro/zadolinnyi/currencyconversionservice/feignClient/CurrencyExchangeServiceProxy.java
@@ -7,10 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service")
+/*@FeignClient(name = "currency-exchange-service")*/
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-    @GetMapping("currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
 }
